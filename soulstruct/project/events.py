39b6cc1bc0b99ea9a0539b90e8548d05073ef371
Grimrukh <dawn.rhapsody@gmail.com>
2020-01-20 17:44:58 -0500

    events: wrong bracket

--- soulstruct/project/events.py
+++ soulstruct/project/events.py
@@ -286,7 +286,7 @@ class SoulstructEventEditor(SmartFrame):
         if not self._ignored_unsaved():
             self.evs_choice.var.set(f"{_get_verbose_map_name(self.selected_evs)} [{self.selected_evs}]")  # keep old
             return
-        self.selected_evs = self.evs_choice.var.get().split(' (')[1][:-1]
+        self.selected_evs = self.evs_choice.var.get().split(' [')[1][:-1]
         self.text_editor.delete(1.0, 'end')
         self.text_editor.insert(1.0, self.evs_text[self.selected_evs])
         self.text_editor.mark_set("insert", "1.0")
