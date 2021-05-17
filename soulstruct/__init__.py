dbe70e66761d20f69d7a9e378e27c211a4001907
Gary Tierney <gary.tierney@fastmail.com>
2021-05-17 09:57:14 +0100

    Use version from setuptools if available

--- soulstruct/__init__.py
+++ soulstruct/__init__.py
@@ -1,10 +1,14 @@
 from pathlib import Path
+from pkg_resources import require
 
 from .base.models.flver import FLVER
 from .config import *
 from soulstruct.containers import Binder
 from soulstruct.containers.dcx import DCX
 
-
-with (Path(__file__).parent / "../VERSION").open("r") as _vfp:
-    __version__ = _vfp.read().strip()
+soulstruct_pkg = require("soulstruct")
+if soulstruct_pkg is not None:
+    __version__ = soulstruct_pkg[0].version
+else:
+    with (Path(__file__).parent / "../VERSION").open("r") as _vfp:
+        __version__ = _vfp.read().strip()
