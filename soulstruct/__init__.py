3f91363a6e4fc2659538c6fd8164ba396bfbae61
Gary Tierney <gary.tierney@fastmail.com>
2021-08-21 02:24:33 +0100

    Fix version checking in non-pip environments

--- soulstruct/__init__.py
+++ soulstruct/__init__.py
@@ -1,14 +1,14 @@
 from pathlib import Path
-from pkg_resources import require
+from pkg_resources import require, DistributionNotFound
 
 from .base.models.flver import FLVER
 from .config import *
 from soulstruct.containers import Binder
 from soulstruct.containers.dcx import DCX
 
-soulstruct_pkg = require("soulstruct")
-if soulstruct_pkg is not None:
+try:
+    soulstruct_pkg = require("soulstruct")
     __version__ = soulstruct_pkg[0].version
-else:
+except DistributionNotFound:
     with (Path(__file__).parent / "../VERSION").open("r") as _vfp:
         __version__ = _vfp.read().strip()
