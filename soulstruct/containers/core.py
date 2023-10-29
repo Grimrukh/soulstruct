c45baf7d2ecbb85c7132a43d99df4174c6e1745d
Scott Mooney <swj.mooney@gmail.com>
2023-10-29 17:27:52 -0400

    Fixed Binder entry exception

--- soulstruct/containers/core.py
+++ soulstruct/containers/core.py
@@ -1142,7 +1142,7 @@ class Binder(BaseBinaryFile):
                 found = entry
         if found:
             return found
-        raise MultipleEntriesFoundError(f"No entry found with `{attr} == {value}`.")
+        raise EntryNotFoundError(f"No entry found with `{attr} == {value}`.")
 
     def find_entry_id(self, entry_id: int, assert_unique=False):
         """Return the first entry with the given `entry_id`.
