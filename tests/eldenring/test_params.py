870a4b83805b9e25fbe49bb81b33dbee612ca45b
Grimrukh <grimrukh@gmail.com>
2025-07-14 16:16:50 +1000

    Added Elden Ring GameParamBND properties

--- tests/eldenring/test_params.py
+++ tests/eldenring/test_params.py
@@ -1,37 +1,21 @@
 """Test reading of encrypted `regulation.bin` (GameParamBND) in Elden Ring."""
-import logging
 import unittest
 
 from soulstruct.logging_utils import setup
 from soulstruct.eldenring.params import GameParamBND
 from soulstruct.utilities.inspection import Timer
-from constrata.metadata import BinaryMetadata
 
 
-# class ParamTest(unittest.TestCase):
-#
-#     def test_atk_param_paramdef(self):
-#         from soulstruct.eldenring.params.paramdef import ParamDef
-#         from soulstruct.utilities.files import SOULSTRUCT_PATH
-#
-#         paramdef = ParamDef.from_paramdex_xml(SOULSTRUCT_PATH("eldenring/params/paramdef/Paramdex/AssetGeometryParam.xml"))
-#         print(f"{paramdef.path_name} total size:", paramdef.get_total_size())
-#
-#     def test_regulation_read(self):
-#         with Timer("Regulation Read"):
-#             regulation = GameParamBND.from_encrypted_path("resources/regulation.bin")
+class ParamTest(unittest.TestCase):
 
+    def setUp(self):
+        setup(console_level="DEBUG")
 
-from soulstruct.utilities.inspection import profile_function
-
-
-@profile_function(20)
-def test_regulation_read():
-    # with Timer("Regulation Read"):
-    regulation = GameParamBND.from_encrypted_path("resources/regulation.bin")
+    def test_regulation_read(self):
+        with Timer("Regulation Read"):
+            regulation = GameParamBND.from_encrypted_path("resources/regulation.bin")
+            print(regulation.NpcParam[47300000])  # Starscourge Radahn
 
 
 if __name__ == '__main__':
-    # unittest.main()
-    setup(console_level="DEBUG")
-    test_regulation_read()
+    unittest.main()
